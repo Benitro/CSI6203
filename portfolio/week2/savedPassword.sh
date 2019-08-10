e43c57435eae6d213226c310cbea3b7b31990f97
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-10 11:37:54 +0000

    Adding Workshop 2 - fixes

--- portfolio/week2/savedPassword.sh
+++ portfolio/week2/savedPassword.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Ask the user for a folder, and pass the input into the variable openFolder
-read -p "Input the name of the folder you would like to view:" openFolder
+read -p "Input the name of the folder you would like to open:" openFolder
 #Open the folder from the input of the variable openFolder
 cd $openFolder
 #Ask the user to input a secret password, and pass the input into the variable savePassword
