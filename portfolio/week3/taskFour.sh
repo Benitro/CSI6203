ee9d4b1854274b074b5c6c200b1d5ccd53964e22
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-13 12:25:43 +0000

    Add workshop 3 - task 4

--- portfolio/week3/taskFour.sh
+++ portfolio/week3/taskFour.sh
@@ -3,10 +3,12 @@ if [ "$#" -ne 3 ]; then
     echo "Error! Number of files passed does not equal to '3'!"
     exit 1
 else
-    if test -e "$*"; then
-        echo "Files found!"
-        echo "$?"
+    folderVar=$(ls)
+    fileVar=$(ls $*)
+    if comm -13 <(ls -f $folderVar) <(ls -f $fileVar); then
+        ls -t $fileVar | head -n1
     else
         echo "Files not found!"
+        exit 1
     fi
 fi
