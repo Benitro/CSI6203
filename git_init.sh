5e76866405ffc70740e1a842726a20955b7c7db1
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-13 02:21:22 +0000

    Next literation of the git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git init
 git add .
-git config --global user.email "bjwalker@our.ecu.edu.au"
-git config --global user.name "bennysgitrepo"
+read -p "Input your git user.email: " userEmail
+git config --global user.email "$userEmail"
+read -p "Input your git user.name: " userName
+git config --global user.name "$userName"
 read -p "Input your git comment: " gitComment
 git commit -m "$gitComment"
 git push -u origin master
