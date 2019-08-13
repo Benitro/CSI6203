0dc9bbc4901cd88f60b72ad91d753a8263fcf2b2
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-13 11:01:56 +0000

    Adding workshop 3 - task 3, 4, and 5

--- portfolio/week3/taskFour.sh
+++ portfolio/week3/taskFour.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+if [ "$#" -ne 3 ]; then
+    echo "Error! Number of files passed does not equal to '3'!"
+    exit 1
+else
+    if test -e "$*"; then
+        echo "Files found!"
+        echo "$?"
+    else
+        echo "Files not found!"
+    fi
+fi
