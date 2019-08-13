e0dec64afd06cea6df8f2d27e34e98510bb9cf97
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-13 02:16:41 +0000

    Add git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git init
-echo "Reinitialized existing Git repository in /home/student/CSI6203/.git/"
 git add .
 git config --global user.email "bjwalker@our.ecu.edu.au"
 git config --global user.name "bennysgitrepo"
