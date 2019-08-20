5e525121d5be9779fd69b74060f68eaed8f7b777
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-20 13:02:20 +0000

    Add week4 workshop

--- portfolio/ChangePermissionForAllWeeks.sh
+++ portfolio/ChangePermissionForAllWeeks.sh
@@ -7,6 +7,7 @@ echo 'Update all permission in weeks to execute recursively'
 chmod +x ~/CSI6203/portfolio/"week1"/*.sh
 chmod +x ~/CSI6203/portfolio/"week2"/*.sh
 chmod +x ~/CSI6203/portfolio/"week3"/*.sh
+chmod +x ~/CSI6203/portfolio/"week4"/*.sh
 cd ~/CSI6203/
 tree
 echo 'Done'
---
+++ portfolio/week4/folderCopier_v2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Author: Ben Walker
+#Edith Cowan University
+
+folderName=""
+# FUNCTION
+IsFolderNameValid() {
+    #if the name is a valid directory
+    if [ -d "$folderName" ]; then
+        #copy it to a new location
+        read -p "Type the name of the destination folder: " newFolderName
+        cp -r "$folderName" "$newFolderName"
+    else 
+        #otherwise, print an error
+        echo "[Folder is invalid] Unable to copy and move folder: " "$folderName"
+        exit 1
+    fi
+}
+
+CopyNewFolderName() {
+    read -p "Type the name of the folder you would like to copy: " folderName
+}
+
+# EBD
+IsFolderNameValid
+CopyNewFolderName
+tree
+
+echo "Done!"
+exit 0
