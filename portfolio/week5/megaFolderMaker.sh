58bf98e9823a76699fb0cef82e6c7b7a7c6271ad
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-26 13:11:41 +0000

    Add workshop 5

--- portfolio/week5/megaFolderMaker.sh
+++ portfolio/week5/megaFolderMaker.sh
@@ -5,10 +5,8 @@ if (( $#!=2 )); then
     echo "Error, provide two numbers" && exit 1
 fi
 
-arg1="$1"
-arg2="$2"
 #For every number between the first argument and the last
-for (( i = $arg1; i <= $arg2; i++))
+for (( i = $1; i <= $2; i++))
 do
     #Create a new folder for that number
     echo "Creating directory number $i"
