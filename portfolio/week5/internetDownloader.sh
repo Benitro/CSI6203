bafdc9301f1368d39e7901ec3381461cb61ab46a
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-08-29 12:55:59 +0000

    Add workshop 5, task 6

--- portfolio/week5/internetDownloader.sh
+++ portfolio/week5/internetDownloader.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+
+#For each ping in range, print the response
 for ip in 'seq 1 254'
 do
 ping -c1 -w1 8.8.8.8
 done
+#If the response = 0, then the Internet is connected!
 if [[ $? -eq 0 ]]; then
     echo "Internet seems to be connected!"
+#If not, then the Internet is down!
 else
     echo "Internet is down!"
 fi
+#Prompt the user to input a website URL, and download the index.html page. Once the user inputs 'exit', close the loop/program
 while :
     do
     read -p "Please type a website URL to download or type 'exit' to quit: " websiteURL
