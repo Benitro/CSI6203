2655020c00ebdcf9fb20f8f7b1e6b5dc8c122bf7
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-04 13:00:13 +0000

    Add Workshop 6

--- portfolio/week6/guessingGame.sh
+++ portfolio/week6/guessingGame.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+#Once the user inputs a number, it will call the function getNumber
 getNumber()
 {
     read -p "$1: "
     while true; do
+        #If the number is less than the input, print a message
         if [ $REPLY -lt $2 ]; then
             echo "Too low!"
             read -p "$1: "
+        #If the number is greater than the input, print a message
         elif [ $REPLY -gt $2 ]; then
             echo "Too high!"
             read -p "$1: "
+        #Else, the input is correct
         else
             echo "Correct!"
             break
+        #End
         fi
     done
 }
