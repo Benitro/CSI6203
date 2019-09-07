24e9d99fec12942e7ddea32293e68a1f52c6af9b
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-07 09:18:33 +0000

    Improve git_init.sh script

--- git_init.sh
+++ git_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git init
 git add .
-read -p "Input your git user.email: " userEmail
-git config --global user.email "$userEmail"
-read -p "Input your git user.name: " userName
-git config --global user.name "$userName"
+#read -p "Input your git user.email: " userEmail
+git config --global user.email "bjwalker@our.ecu.edu.au"
+#read -p "Input your git user.name: " userName
+git config --global user.name "bennysgitrepo"
 read -p "Input your git comment: " gitComment
 git commit -m "$gitComment"
 git push -u origin master
