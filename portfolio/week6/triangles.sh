3f975d59901762867b58167fca8f6339f3935f6e
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-08 13:29:11 +0000

    Saving..

--- portfolio/week6/triangles.sh
+++ portfolio/week6/triangles.sh
@@ -2,14 +2,8 @@
 
 baseTriangle()
 {
-    b=1
-    h=1
-    while (( 1 < $b || 100 > $h )); do
-    area=$(($b*$h/2))
-    echo "The area for triangle with base: $b and height: $h is: $area"
-    b=$((b+1))
-    h=$((h+1))
-    done
+    area=$(($1*$2/2))
+    echo "The area for triangle with base: $1 and height: $2 is: $area"
 }
 
-baseTriangle
+baseTriangle 1 2
