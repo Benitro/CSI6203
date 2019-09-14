b782d8a649e3861e86de534e656d724c18701926
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-14 04:02:38 +0000

    Saving Workshop 6..

--- portfolio/week6/internetDownloadFunc.sh
+++ portfolio/week6/internetDownloadFunc.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#Fuction to download the webpage, and store into the choosen directory. The output of the wget response is limited using -nv
 internetDownload()
 {
-    wget http://$saveUrl -P ~/$locFile
+    wget -nv http://$saveUrl -P ~/$locFile
 }
 
+#Print the message
 echo "Running the download function:"
+#Prompt the user to specify the webpage they would like to download
 read -p "Type the URL of a file to download: " saveUrl
+#Prompt the user to specify the folder location they would like to save it to
 read -p "Type the location of where you would like to download the file: " locFile
+#Call the function, passing the input from the user
 internetDownload
+#Print the message below, and exit
 echo "Download function complete!"
+exit 0
--- portfolio/week6/triangles.sh
+++ portfolio/week6/triangles.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+#Function for calculating the area of a triangle
 baseTriangle()
 {
-    area=$(($1*$2/2))
-    echo "The area for triangle with base: $1 and height: $2 is: $area"
+    area=$((b*h/2))
+    echo "The area for triangle with base: $b and height: $h is: $area"
 }
-for (( i=0; i<=20; i++))
+
+b=1
+h=1
+
+#For literation of 1 - 100, call the triangle function, and if height less than 10, add +1, else add 1+ to base 
+for (( i=0; i<100; i++))
 do
-    baseTriangle 1 4
+    baseTriangle
+    if [ $h -lt 10 ]; then
+        h=$((h+1))
+    elif [ $h -le 10 ]; then
+        h=$((1))
+        b=$((b+1))
+    fi
 done
+exit 0
