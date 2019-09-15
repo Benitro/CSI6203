384ef69db278896cfb8e25d665ca8df0be704930
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-15 12:04:36 +0000

    Saving..

--- portfolio/week7/shorterTriangles.sh
+++ portfolio/week7/shorterTriangles.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-triangles=~/CSI6203/portfolio/week6/triangles.sh
+triangles=$(~/CSI6203/portfolio/week6/triangles.sh)
 
-shortTri=$(echo "$triangles" | sed 's/base/Base: /p')
+#shortTri=$(echo "$triangles" | sed -n '/The area for triangle with / {
+#s/base/Base/
+#s/height/Height/
+#s/is/Area/
+#p
+#}')
 
-$shortTri
+shortTri=$(echo "$triangles" | sed 's/^.*\(base.*\)/\1/g {
+s/base/Base/
+s/height/Height/
+s/is/Area/
+p
+}')
+
+echo -e "$shortTri"
