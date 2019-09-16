5bae7abe0cecc105f8cf334083946c5e5079ebcf
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-16 12:53:07 +0000

    Adding Workshop 7..

--- portfolio/week7/shorterTriangles.sh
+++ portfolio/week7/shorterTriangles.sh
@@ -4,6 +4,7 @@ triangles=$(~/CSI6203/portfolio/week6/triangles.sh)
 
 shortTri=$(echo "$triangles" | sed -n '/.*The area for triangle with.* / {
 s/.*base/Base/
+s/and*//
 s/height/Height/
 s/is/Area/
 p
