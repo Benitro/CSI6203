bf951abf3de059f68a1cad5fc718b5d93e2ffbbb
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-21 05:31:47 +0000

    Adding Workshop 8..

--- portfolio/ChangePermissionForAllWeeks.sh
+++ portfolio/ChangePermissionForAllWeeks.sh
@@ -10,6 +10,11 @@ chmod +x ~/CSI6203/portfolio/"week3"/*.sh
 chmod +x ~/CSI6203/portfolio/"week4"/*.sh
 chmod +x ~/CSI6203/portfolio/"week5"/*.sh
 chmod +x ~/CSI6203/portfolio/"week6"/*.sh
+chmod +x ~/CSI6203/portfolio/"week7"/*.sh
+chmod +x ~/CSI6203/portfolio/"week8"/*.sh
+chmod +x ~/CSI6203/portfolio/"week9"/*.sh
+chmod +x ~/CSI6203/portfolio/"week10"/*.sh
+chmod +x ~/CSI6203/portfolio/"week11"/*.sh
 cd ~/CSI6203/
 tree
 echo 'Done'
--- portfolio/week8/regexes.sh
+++ portfolio/week8/regexes.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-grep -r "sed"
+#All sed statements
+grep -r 'sed'
+
+#All lines that starts with the letter m
+grep -r '^m'
+
+#All lines that contain three digit numbers
+grep -r '[0-9]\{3\}'
+
+#All lines that start with a vowel
+grep -r '^[aeiou]'
+
+#All lines that contains loops
+
+
+#All echo statements with at least three words
