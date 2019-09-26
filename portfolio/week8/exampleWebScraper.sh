76831b31873135097a0b37dd5aa693b4fb165b47
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-26 12:29:20 +0000

    Adding Workshop 8..

--- portfolio/week8/exampleWebScraper.sh
+++ portfolio/week8/exampleWebScraper.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-exampleWeb=$(curl -s http://example.com)
+#exampleWeb=$(curl -s http://example.com)
 
-echo $exampleWeb
+#echo $exampleWeb
+
+title=$(curl -s http://example.com | grep "<h1>" | sed -n "/<h1>/,/<\/h1>/p")
+
+paragraph=$(curl -s http://example.com | grep "<p>" | sed -n "/<p>/,/<\/p>/p")
+
+echo -e "$title\n$paragraph"
