db0d00b70567570328561a204938bab771b00901
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-30 10:04:12 +0000

    Adding Assignment 2..

--- assignment/nasa.sh
+++ assignment/nasa.sh
@@ -1,49 +1,34 @@
 #!/bin/bash
 
+#Author: Benjamin Jacob Walker
+#Edith Cowan University
+
 function storeContent() {
-    local formatDate=$(date "$1" $2 +%y%m%d)
+    local formatDate=$(date -d $2 +%y%m%d)
     local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
     local content=$(curl -s $url)
     echo "$content"
 }
 
 function grabImageNameTitle() {
-    #local formatDate=$(date "$1" $2 +%y%m%d)
-    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
-    #local content=$(curl -s $url)
     local imageName=$(echo $@ | grep -o "<b>.*</b> <br>" | sed 's/<[^>]*>//g')
     echo $imageName
 }
 
 function grabExplanation() {
-    #local formatDate=$(date "$1" $2 +%y%m%d)
-    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
-    #local content=$(curl -s $url)
     local explain=$(echo $@ | grep -o "<b> Explanation.*<p>.*<center>" | sed 's/<[^>]*>//g')
     echo $explain
 }
 
 function grabImageCredit() {
-    #local formatDate=$(date "$1" $2 +%y%m%d)
-    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
-    #local content=$(curl -s $url)
     local credit=$(echo $@ | grep -o "Image Credit.*</center> <p>" | sed 's/<[^>]*>//g')
     echo $credit
 }
 
 function grabImageWebLink() {
-    #local formatDate=$(date "$1" $2 +%y%m%d)
-    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
-    #local content=$(curl -s $url)
     local extension=$(echo $@ | grep -o 'image/.*<I' | sed 's/<I/>/g' | tr -d '>"')
     local addExtension="https://apod.nasa.gov/apod/"$extension
     echo $addExtension
 }
-
-#storeContent $1 $2
-#grabImageNameTitle $1 $2
-#grabExplanation $1 $2
-#grabImageCredit $1 $2
-#grabImageWebLink $1 $2
 
 #Notes: Testing video content ..apod/ap190108
