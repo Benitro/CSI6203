dfd773f54ddc9e7d5c3e74b313d6e924642e80a4
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-09-30 06:53:20 +0000

    Adding Assignment 2..

--- assignment/nasa.sh
+++ assignment/nasa.sh
@@ -1,33 +1,49 @@
 #!/bin/bash
 
 function storeContent() {
-if [[ $? -eq 0 ]]; then
-    echo "Connecting to nasa.gov..."
     local formatDate=$(date "$1" $2 +%y%m%d)
     local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
     local content=$(curl -s $url)
     echo "$content"
-else
-    echo "Cannot connect to nasa.gov..."
-fi
 }
 
-function grabImageName() {
-    local imageName=$(echo $@ | grep -o "<b>.*</b> <br>" | sed 's/<[^>]*>//g' | head -1)
+function grabImageNameTitle() {
+    #local formatDate=$(date "$1" $2 +%y%m%d)
+    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
+    #local content=$(curl -s $url)
+    local imageName=$(echo $@ | grep -o "<b>.*</b> <br>" | sed 's/<[^>]*>//g')
     echo $imageName
 }
 
 function grabExplanation() {
-    local explain=$(echo $@ | grep -o "<b> Explanation.*<p>.*</a> <br> <b>" | sed 's/<[^>]*>//g')
+    #local formatDate=$(date "$1" $2 +%y%m%d)
+    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
+    #local content=$(curl -s $url)
+    local explain=$(echo $@ | grep -o "Explanation.*<p>.*<center>" | sed 's/<[^>]*>//g')
     echo $explain
 }
 
 function grabImageCredit() {
-    local credit=$(echo $@ | grep -o "<b> Image Credit.* </center> <p>" | sed 's/<[^>]*>//g' | head -1)
+    #local formatDate=$(date "$1" $2 +%y%m%d)
+    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
+    #local content=$(curl -s $url)
+    local credit=$(echo $@ | grep -o "Image Credit.*</center> <p>" | sed 's/<[^>]*>//g')
     echo $credit
 }
 
-#storeContent $1 $2
-#grabImageName $1 $2
+function grabImageWebLink() {
+    #local formatDate=$(date "$1" $2 +%y%m%d)
+    #local url="https://apod.nasa.gov/apod/ap"$(echo "$formatDate")".html"
+    #local content=$(curl -s $url)
+    local extension=$(echo $@ | grep -o 'image/.*<I' | sed 's/<I/>/g' | tr -d '>"')
+    local addExtension="https://apod.nasa.gov/apod/"$extension
+    echo $addExtension
+}
+
+storeContent $1 $2
+#grabImageNameTitle $1 $2
 #grabExplanation $1 $2
 #grabImageCredit $1 $2
+#grabImageWebLink $1 $2
+
+#Notes: Testing video content ..apod/ap190108
