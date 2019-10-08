21c8c56cafb62e89c5e3e24d7f392830d24e4910
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-10-08 13:01:23 +0000

    Adding Workshop 9..

--- portfolio/week9/shoppingList.sh
+++ portfolio/week9/shoppingList.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 echo "Your shopping list is as follows:"
-awk 'BEGIN {FS=","; currency="$"; print "Item | Quantity | Price"}
-{printf("%-15s | %-10s| %-10s\n", $1, $2, $3)}' shopping.csv
-
-echo "Your shopping list is as follows:"
-cat shopping.csv | column -t -s ","
+awk 'BEGIN {FS=","; currency="$"; print "Item            | Quantity  | Price"; print "                |           |      "; print "-------------------------------------"}
+{printf("%-15s | %-10s| %-10s\n", $1, $2, currency $3)}' shopping.csv
