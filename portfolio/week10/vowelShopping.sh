b912b2f9bbf7dc10fc02ddf8325b059718393ee5
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-10-15 13:12:29 +0000

    Adding Workshop 10..

--- portfolio/week10/vowelShopping.sh
+++ portfolio/week10/vowelShopping.sh
@@ -14,6 +14,6 @@ awk 'BEGIN {
     /[a-z][[:blank:]][a-z]*/ {
         print "has more than one word: " $1
         }
-    /[0-9]+ $3/ {
-        print "whole number price: " $3
+    /.*,.*,[0-9]+$/ {
+        print "whole number price: " $1
         }' ../week9/shopping.csv
