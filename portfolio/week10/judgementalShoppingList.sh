58a1328a01f5f8d80ceec6d5fdf1baa15477df6e
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-10-16 13:31:18 +0000

    Adding Workshop 10..

--- portfolio/week10/judgementalShoppingList.sh
+++ portfolio/week10/judgementalShoppingList.sh
@@ -2,22 +2,28 @@
 
 #Print the CSV file into a table, displaying the total for each row, and column
 
-awk 'function comment(digit){
-if($2>5)
-    printf "Do you really need that many?" digit
-else if($2*$3 > 5)
-    printf "That’s a bit expensive! Are you sure?" digit
-else if($2*$3 < 3)
-    printf "Such a cheapskate!" digit
-}'
-
 echo "Your shopping list is as follows:"
 awk 'BEGIN {FS=","; currency="$";
 print "Item            | Quantity  | Price     | Total     | Comment       "; 
 print "                |           |           |           |               "; 
 print "--------------------------------------------------------------------"}
+{if($2>5){
+    printf "Do you really need that many?"
+    }
+else if($2*$3 > 5){
+    printf "That’s a bit expensive! Are you sure?"
+    }
+else if($2*$3 < 3){
+    printf "Such a cheapskate!"}}
 {sum+=$2}
 {count+=$3}
 {counter+=$2*$3}
-{printf("%-15s | %-10s| %-10s| %-10s| %-10s\n", $1, $2, currency $3, currency $2*$3, comment($0))}
+{printf("%-15s | %-10s| %-10s| %-10s| %-10s\n", $1, $2, currency $3, currency $2*$3, $5)}
 END {printf("%-15s | %-10s| %-10s| %-10s\n", "TOTAL:", sum, currency count, currency counter)}' ../week9/shopping.csv
+
+#if($2>5)
+#    printf "Do you really need that many?"
+#else if($2*$3 > 5)
+#    printf "That’s a bit expensive! Are you sure?"
+#else if($2*$3 < 3)
+#    printf "Such a cheapskate!"
