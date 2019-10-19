ba02339b8f5aa222728cdcf8f7c32ad65f9e4bfc
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-10-19 05:39:25 +0000

    Adding Workshop 10..

--- portfolio/week10/authLogReader.sh
+++ portfolio/week10/authLogReader.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/var/log/auth.log
+#Print Auth Log: followed by the positions in the auth.log file; <date>, <time>, source:, message:
+
+echo -e "Auth Log:"
+awk '/^/ {print $1 " " $2 " - " $3 ":"}
+{print $5}
+{message=" "; for(i=6;i<=NF;i++){message=message" "$i}; printf("\t%-15s\n\n", message)}' /var/log/auth.log
