a5fb8a7d6aa6916674cc1315a2ffd478c4069c2f
bennysgitrepo <bjwalker@our.ecu.edu.au>
2019-10-19 14:18:53 +0000

    Adding Workshop 10..

--- portfolio/week10/advancedScraper.sh
+++ portfolio/week10/advancedScraper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 content=$(curl -s https://www.ecu.edu.au/degrees/study-areas/science)
-title=$(echo $content | grep -o '<.*>\|^--.*' | sed 's/<[^>]*>//g')
+title=$(echo $content | grep -o '<a href="https://www.ecu.edu.au/degrees/study-areas/science/[a-zA-Z][a-zA-Z]*" title="[a-zA-Z][a-zA-Z]*">[a-zA-Z][a-zA-Z]*</a>' | sed 's/<[^>]*>//g')
 echo $title
