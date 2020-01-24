00fd95eb335e9c06f1907d3734ff7772fbbd746b
Ben <53471022+bennysgitrepo@users.noreply.github.com>
2020-01-24 17:17:34 +0800

    Updating git_init file

--- git_init.sh
+++ git_init.sh
@@ -2,9 +2,9 @@
 git init
 git add .
 #read -p "Input your git user.email: " userEmail
-git config --global user.email "bjwalker@our.ecu.edu.au"
+git config --global user.email "insert email here"
 #read -p "Input your git user.name: " userName
-git config --global user.name "bennysgitrepo"
+git config --global user.name "insert user name here"
 read -p "Input your git comment: " gitComment
 git commit -m "$gitComment"
 git push -u origin master
